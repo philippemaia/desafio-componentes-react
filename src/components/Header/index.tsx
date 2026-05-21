86a24ff236f721c5721167b9b7492367013fe89d
Philippe Maia <philippe.m.maia@hotmail.com>
2026-05-21 00:09:24 -0300

    Organizando os componentes do Layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,6 @@ import './styles.css';
 
 export default function Header(){
     return(
-        // <h2 className="h2-header">Cabeçalho</h2>
         <header>
             <nav className="container">
                 <h1>DSCarrosTop</h1>
--- src/routes/Car/index.tsx
+++ src/routes/Car/index.tsx
@@ -1,32 +1,31 @@
-import CarCard from '../../components/CarCard';
-import CommentCard from '../../components/CommentCard ';
-import Footer from '../../components/Footer';
-import Header from '../../components/Header';
-import './styles.css';
+import CarCard from "../../components/CarCard";
+import CommentCard from "../../components/CommentCard ";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import "./styles.css";
 
-export default function Car(){
-
-    return(
-        <>
-        <Header />
-        <section id="cars">
+export default function Car() {
+  return (
+    <>
+      <Header />
+      <section id="cars">
         <div className="container">
-            <h2>Venha nos visitar</h2>
-            <CarCard />
-            <CarCard />
+          <h2>Venha nos visitar</h2>
+          <CarCard />
+          <CarCard />
         </div>
-        </section>
-        <section id="comments">
-            <div className="container">
-            <h2>O que estão dizendo</h2>
-            <CommentCard />
-            <CommentCard />
-            <CommentCard />
-            <CommentCard />
-            <CommentCard />
-            </div>
-        </section>
-        <Footer />
-        </>
-    );
+      </section>
+      <section id="comments">
+        <div className="container">
+          <h2>O que estão dizendo</h2>
+          <CommentCard />
+          <CommentCard />
+          <CommentCard />
+          <CommentCard />
+          <CommentCard />
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
